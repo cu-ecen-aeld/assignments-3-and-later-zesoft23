b7af6ad5e9758fc888910e6b962d4555233f2eb9
Jack Thomson <thomsonjack41@gmail.com>
2024-03-23 23:30:14 -0400

    Fix syslog in writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <syslog.h>
 
-
-
 // arg 1 is path to file
 // arg 2 is string to write to file
 int main(int argc, char **argv)
 {
-    if (argc < 3) {
-        syslog(LOG_ERR, "Too few args passed");
+    openlog(NULL, 0, LOG_USER);
+
+    if (argc < 3)
+    {
+        syslog(LOG_ERR, "Invalid Number of arguments: %d", argc);
         printf("Too few args passed\n");
         printf("Usage: %s filepath writestring\n", argv[0]);
         return 1;
     }
-    openlog("writer", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_USER);
 
-    FILE* myfile = fopen(argv[1], "w");
+    FILE *myfile = fopen(argv[1], "w");
     fprintf(myfile, argv[2]);
-    syslog(LOG_DEBUG, "Writing %s to %s\n", argv[1], argv[2]);
+    syslog(LOG_DEBUG, "Writing %s to %s", argv[1], argv[2]);
     fclose(myfile);
 
     closelog();
     return 0;
 }
-
