4f2c178f2cf2d9029256b34f823be6842716c372
Jack Thomson <thomsonjack41@gmail.com>
2024-04-05 12:42:15 -0400

    Add working linux kernel build script and update finder scripts to be sh compatible

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 usage() { echo "Usage: $0 filedir searchstr" 1>&2; exit 1; }
 
@@ -6,15 +6,15 @@ usage() { echo "Usage: $0 filedir searchstr" 1>&2; exit 1; }
 # $2 is the string to search for in the directory
 
 # Argument checking
-if [[ $# -lt 2 ]]; then
+if [ $# -lt 2 ]; then
     echo "Too few arguments passed in"
     usage
     exit 1
-elif [[ $# -gt 2 ]]; then
+elif [ $# -gt 2 ]; then
     echo "Too many arguments passed in"
     usage
     exit 1
-elif [[ ! -d $1 ]]; then
+elif [ ! -d $1 ]; then
     echo $1
     echo "File directory does not exist or is not a directory"
     usage
@@ -27,5 +27,3 @@ file_count="$(find $1 -type f | wc -l)"
 line_count="$(grep -r $2 $1 | wc -l)"
 
 echo "The number of files are $file_count and the number of matching lines are $line_count"
-
-
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script outline to install and build kernel.
-# Author: Siddhant Jajoo.
+# Author: Siddhant Jajoo, Jack Thomson
 
 set -e
 set -u
@@ -12,6 +12,9 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+CROSS_COMPILE_PATH="$(which aarch64-none-linux-gnu-gcc)"
+CROSS_COMPILE_LOCATION="$(dirname $CROSS_COMPILE_PATH)/../"
+BASE_DIR=$(dirname $(realpath -s $0))
 
 if [ $# -lt 1 ]
 then
@@ -28,16 +31,22 @@ if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+    # Removes the extra yylloc line
+    sed --in-place --expression="41d" $OUTDIR/linux-stable/scripts/dtc/dtc-lexer.l
 fi
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE mrproper
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
+    make -j4 ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE all
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE dtbs
 fi
 
 echo "Adding the Image in outdir"
+ln -sf ${OUTDIR}/linux-stable/arch/arm64/boot/Image ${OUTDIR}/Image
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,7 +56,23 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Create neccessary base dirs
+mkdir -p "${OUTDIR}/rootfs/bin"
+mkdir -p "${OUTDIR}/rootfs/dev"
+mkdir -p "${OUTDIR}/rootfs/etc"
+mkdir -p "${OUTDIR}/rootfs/home"
+mkdir -p "${OUTDIR}/rootfs/lib"
+mkdir -p "${OUTDIR}/rootfs/lib64"
+mkdir -p "${OUTDIR}/rootfs/proc"
+mkdir -p "${OUTDIR}/rootfs/sys"
+mkdir -p "${OUTDIR}/rootfs/sbin"
+mkdir -p "${OUTDIR}/rootfs/tmp"
+mkdir -p "${OUTDIR}/rootfs/usr"
+mkdir -p "${OUTDIR}/rootfs/var/log"
+mkdir -p "${OUTDIR}/rootfs/usr/bin"
+mkdir -p "${OUTDIR}/rootfs/usr/sbin"
+mkdir -p "${OUTDIR}/rootfs/root"
+
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +80,51 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+make distclean
+make defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# Add program intepreter to lib folder
+cp $CROSS_COMPILE_LOCATION/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
 
-# TODO: Make device nodes
+# Add library dependencies to rootfs
+cp $CROSS_COMPILE_LOCATION/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
+cp $CROSS_COMPILE_LOCATION/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
+cp $CROSS_COMPILE_LOCATION/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
 
-# TODO: Clean and build the writer utility
+# Make device nodes
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 5
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/console c 5 1
+
+# Clean and build the writer utility
+cd $BASE_DIR
+make clean
+make CROSS_COMPILE=$CROSS_COMPILE
+
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
 
-# TODO: Chown the root directory
+cp -r *.sh $OUTDIR/rootfs/home/
+cp writer $OUTDIR/rootfs/home/
+cp -r ../conf $OUTDIR/rootfs/home/
 
-# TODO: Create initramfs.cpio.gz
+# Chown the root directory
+sudo chown -R $UID $OUTDIR/rootfs
+
+# Create initramfs.cpio.gz
+
+cd "$OUTDIR/rootfs"
+
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
